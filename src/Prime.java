5043a606f96ba4d300451071d7f0aa54a9931b87
Ramyavenkat <ramyapoorni@gmail.com>
2019-06-28 13:58:43 +0530

    series pgm

---
+++ src/Prime.java
@@ -0,0 +1,16 @@
+
+public class Prime {
+	public static void main(String[] args) {
+		int a = 0, b = 1;
+		System.out.println(a);
+		System.out.println(b);
+		for (int i = 1; i <=10; i++) {
+			int c=a+b;
+			System.out.println(c);
+			a=b;
+			b=c;
+			
+		}
+
+	}
+}
